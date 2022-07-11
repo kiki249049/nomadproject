95f16749936dbc50cc5333b11b1c5f339a8cbac2
kiki249 <kiki249049@gmail.com>
2022-07-12 02:20:22 +0900

    up

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import Movie from "../components/Movie.js"
 import { useState, useEffect } from 'react';
+import styles from './Home.module.css'
 
 function Home() {
   const URL = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
@@ -18,11 +19,20 @@ function Home() {
   console.log(movies)
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : <div>
-        {movies.map((movie) => 
-        <Movie id={movie.id} key={movie.id} medium_cover_image={movie.medium_cover_image} title={movie.title} genres={movie.genres} summary={movie.summary} />)}
+      <div className={styles.container}>
+        {loading ? <h1>Loading...</h1> : <div className={styles.movies}>
+          {movies.map((movie) => 
+          <Movie 
+          id={movie.id} 
+          key={movie.id} 
+          medium_cover_image={movie.medium_cover_image}
+          year={movie.year} 
+          title={movie.title} 
+          genres={movie.genres} 
+          summary={movie.summary} />)}
+        </div>
+        }
       </div>
-      }
     </div>
   )
 }
